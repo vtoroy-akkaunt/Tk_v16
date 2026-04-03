3c9b5c4781f50da2b9c0f99a31fef83b1f75fa29
234912 <234912@edu.fa.ru>
2026-04-03 15:02:00 +0300

    Добавьте файлы проекта.

---
+++ Tk_v16/Calc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tk_v16
+{
+    // Допустим, что спецификация для этого класса составлена именно так, что они делают настолько мало, как тут
+    internal class Calc
+    {
+        /// <summary>
+        /// Вычисляет ток по данным показаниям напряжения и сопротивления. Кидает исключения при некорректном вводе, тип исключений не уточняется.
+        /// </summary>
+        /// <param name="volt">Напряжение в вольтах</param>
+        /// <param name="ohm">Сопротивление в омах</param>
+        /// <returns>Ток в амперах</returns>
+        public static double current(double volt, double ohm)
+        {
+            return volt / ohm;
+        }
+        /// <summary>
+        /// Вычисляет напряжение по данным показаниям сопротивления и тока. Кидает исключения при некорректном вводе, тип исключений не уточняется.
+        /// </summary>
+        /// <param name="ohm">Сопротивление в омах</param>
+        /// <param name="ampere">Ток в амперах</param>
+        /// <returns>Напряжение в вольтах</returns>
+        public static double voltage(double ohm, double ampere)
+        {
+            return ohm * ampere;
+        }
+        /// <summary>
+        /// Вычисляет напряжение по данным показаниям сопротивления и тока. Кидает исключения при некорректном вводе, тип исключений не уточняется.
+        /// </summary>
+        /// <param name="ampere">Ток в амперах</param>
+        /// <param name="volt">Напряжение в вольтах</param>
+        /// <returns>Сопротивление в омах</returns>
+        public static double resistance(double ampere, double volt)
+        {
+            return volt / ampere;
+        }
+    }
+}
---
+++ Tk_v16/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tk_v16
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void rb_changed(object sender, RoutedEventArgs e)
+        {
+            if (rb_current.IsChecked == true)
+            {
+                label1.Text = "Напряжение (Вольт)";
+                label2.Text = "Сопротивление (Ом)";
+            } else
+            if (rb_voltage.IsChecked == true)
+            {
+                label1.Text = "Сопротивление (Ом)";
+                label2.Text = "Сила тока (ампер)";
+            } else
+            if (rb_resistance.IsChecked == true)
+            {
+                label1.Text = "Сила тока (ампер)";
+                label2.Text = "Напряжение (Вольт)";
+            } else
+            {
+                // Такого не может быть никогда (c)
+                throw new Exception("Не выбран ни один вариант");
+            }
+        }
+        private string format(double x)
+        {
+            return string.Format("0,000", x);
+        }
+        private void btn_calc_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double x1 = Convert.ToDouble(label1.Text);
+                double x2 = Convert.ToDouble(label2.Text);
+                if (rb_current.IsChecked == true)
+                {
+                    result.Text = format(Calc.current(x1, x2));
+                }
+                else
+                if (rb_voltage.IsChecked == true)
+                {
+                    result.Text = format(Calc.voltage(x1, x2));
+                }
+                else
+                if (rb_resistance.IsChecked == true)
+                {
+                    result.Text = format(Calc.resistance(x1, x2));
+                }
+                else
+                {
+                    // Такого не может быть никогда (c)
+                    throw new Exception("Не выбран ни один вариант");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Некорректный ввод");
+            }
+        }
+    }
+}
